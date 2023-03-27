1bc05dc89eaffaf8941faa33c395cd02277de7b9
Pedr0Leite <pedromiguelgoncalvesleite@gmail.com>
2023-03-27 14:40:10 +0100

    Navbar fixed and clock almost done

---
+++ components/WorksBlock/WorksBlock.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+interface WorksBlockInterface {
+    
+}
+
+export default function WorksBlock() {
+  return (
+    <div className="worksGrid">
+      <div className="worksRowBlock1">
+        <div className="worksBlock1">
+          <div className="figure">
+            <p>
+              <img src="https://via.placeholder.com/710x810" />
+            </p>
+            <p>
+              <span>Altava</span>
+              <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+            </p>
+          </div>
+        </div>
+        <div className="worksBlock2">
+          <div className="figure">
+            <p>
+              <img src="https://via.placeholder.com/454x255" />
+            </p>
+            <p>
+              <span>Altava</span>
+              <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+            </p>
+          </div>
+          <div className="figure">
+            <p>
+              <img src="https://via.placeholder.com/414x414" />
+            </p>
+            <p>
+              <span>Altava</span>
+              <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="worksRowBlock2">
+        <div className="figure">
+          <p>
+            <img src="https://via.placeholder.com/336x336" />
+          </p>
+          <p>
+            <span>Altava</span>
+            <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+          </p>
+        </div>
+        <div className="figure">
+          <p>
+            <img src="https://via.placeholder.com/394x220" />
+          </p>
+          <p>
+            <span>Altava</span>
+            <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+          </p>
+        </div>
+        <div className="figure">
+          <p>
+            <img src="https://via.placeholder.com/395x500" />
+          </p>
+          <p>
+            <span>Altava</span>
+            <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+          </p>
+        </div>
+      </div>
+      <div className="worksRowBlock3">
+        <div className="figure">
+          <p>
+            <img src="https://via.placeholder.com/1361x766" />
+          </p>
+          <p>
+            <span>Altava</span>
+            <span>ART DIRECTION, DESIGN, ILLUSTRATION</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/works/[id].tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+
+
+export default function WorksDetails() {
+  return (
+    <div className="worksDetailsMainDiv">
+      <div className="worksDetailsLeft">
+        <div className="worksDetailsTitle">
+          <div>Altava Group</div>
+        </div>
+        <div className="worksDetailsInfo">
+            <span>Client</span>
+            <span>Altava Group, Your Majesty</span>
+        </div>
+        <div className="worksDetailsInfo">
+            <span>Year</span>
+            <span>2022</span>
+
+        </div>
+        <div className="worksDetailsInfo">
+            <span>Location</span>
+            <span>Amsterdam / Hong Kong / L.A.</span>
+
+        </div>
+        <div className="worksDetailsInfo">
+            <span>Services</span>
+            <span>Art Direction, Visual Design, Typography</span>
+
+        </div>
+        <div className="worksDetailsText">
+          Ricardo Leite is an Amsterdam-based designer and art director, with a
+          strong typography passion and identity-driven profile, bringing
+          emotion and character into Design. Since 2010, he has worked on global
+          projects as art director, designer, and illustrator for arts, culture,
+          and advertising clients. Specializing in the development of creative
+          strategies and visual concepts for sports, fashion, food, tech, and
+          music fields. Design & Art direction as a tool to create visual
+          narratives. From local brands to global briefings.
+        </div>
+      </div>
+      <div className="worksDetailsRight">
+      <img src="https://via.placeholder.com/700x382" />
+      <img src="https://via.placeholder.com/690x378" />
+      <img src="https://via.placeholder.com/692x367" />
+
+      </div>
+    </div>
+  );
+}
